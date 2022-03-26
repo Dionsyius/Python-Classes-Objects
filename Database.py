c4377349ea160c50f8dce172896757d593abcefd
Dionsyius <oraphael@email.essex.edu>
2022-03-26 16:35:17 -0400

    Classes and Objects

---
+++ Database.py
@@ -0,0 +1,179 @@
+class School:
+ def introduce_self(self):
+  print("                                     " +self.Essex)
+  print("             " + self.Program)
+  print("    " + self.GER0)
+  print(" " + self.GER1)
+  print(" " + self.GER2)
+  print(" " + self.GER3)
+  print("        " + self.GER4)
+  print(" " + self.GER5)
+  print(" " + self.GER6)
+  print(" " + self.GER7)
+  print(" " + self.GER8)
+  print(" " + self.GER9)
+  print(" " + self.GER10)
+  print(" " + self.GER11)
+  print(" " + self.GER12)
+  print(" " + self.GER13)
+  print(" " + self.GER14)
+  print(" " + self.GER15)
+  print(" " + self.GER16)
+  print(" " + self.GER17)
+  print("         " + self.GER18)
+  print(" " + self.GER19)
+  print(" " + self.GER20)
+  print(" " + self.GER21)
+  print(" " + self.GER22)
+  print(" " + self.GER23)
+  print(" " + self.GER24)
+  print(" " + self.GER25)
+  print(" " + self.GER26)
+  print(" " + self.GER27)
+  print(" " + self.GER28)
+  print(" " + self.GER29)
+  print("        " + self.GER30)
+  print(" " + self.GER31)
+  print(" " + self.GER32)
+  print(" " + self.GER33)
+  print(" " + self.GER34)
+  print(" " + self.GER35)
+  print(" " + self.GER36)
+
+  print("Total Credits Required " + self.Credits)
+  print("             " + self.FirstSemester)
+  print("Name :  " + self.Name)
+  print("Studentid : " + self.Studentid)
+  print("Department : " + self.Department)
+  print("Class : " + self.ClassEng)
+  print("Class : " + self.ClassCSC)
+  print("Class : " + self.ClassMTH)
+  print("Class : " + self.ClassCSC137)
+
+
+r1 = School()
+r1.Essex = "WELCOME TO ESSEX COUNTY COLLEGE"
+r1.Program = "Program Requirements"
+r1.GER0 = "-GENERAL EDUCATION REQUIREMENTS"
+r1.GER1 = "-Written & Oral Communication (6 credits)"
+r1.GER2 = "ENG101 (3 credits)"
+r1.GER3 = "ENG102 or ENG105 (one 3-credit course)"
+
+
+
+r1.GER4 = "-Quantitative Knowledge & Skills and Scientific Knowledge and Reasoning (8 credits)"
+r1.GER5 = "MTH100 (4 credits) & Choose one mathematics course or one lab science course from the following : "
+r1.GER6 = "MTH113"
+r1.GER7 = "MTH119"
+r1.GER8 = "MTH120"
+r1.GER9 = "MTH127"
+r1.GER10 = "MTH136"
+r1.GER11 = "MTH239"
+r1.GER12 = "BIO101"
+r1.GER13 = "BIO122"
+r1.GER14 = "CHM101"
+r1.GER15 = "CHM104"
+r1.GER16 = "PHY101"
+r1.GER17 = "PHY113 OR PHY114 (one 4-credit course)"
+
+
+
+r1.GER18 = "Society & Human Behavior (3 credits)"
+r1.GER19 = "Choose one of the following courses : "
+r1.GER20 = "ANT101"
+r1.GER21 = "ANT105"
+r1.GER22 = "EC0101"
+r1.GER23 = "ECO102"
+r1.GER24 = "POL101"
+r1.GER25 = "POL104"
+r1.GER26 = "PSY101"
+r1.GER27 = "PSY219"
+r1.GER28 = "SOC101"
+r1.GER29 = "SOC219 (one 3-credit course)"
+
+
+
+r1.GER30 = "Historical Perspective (3 credits)"
+r1.GER31 = "Choose one of the following history courses :"
+r1.GER32 = "HST 101"
+r1.GER33 = "HST 121"
+r1.GER34 = "HST 131"
+r1.GER35 = "HST 137"
+r1.GER36 = "HST 161 or HST 162 (one 3-credit course)"
+
+
+r1.Credits = "61-63"
+r1.FirstSemester = "First Semester"
+r1.Name = "Raphael Obumnaenye Odinamkpa"
+r1.Studentid = "900232940"
+r1.Department = "Cybersecurity and Network tech"
+r1.ClassEng = "College Composition 1 (3 credits)"
+r1.ClassCSC = "Network Fundamentals (3 credits)"
+r1.ClassMTH = "Introductory College Mathematics (4 credits)"
+r1.ClassCSC137 = "Introduction to Programming In Java (4 credits)"
+r1.introduce_self()
+
+class Second:
+ def introduce_self(self):
+  print("             " + self.SecondSemester)
+  print("Class : " + self.ClassCSC113)
+  print("Class : " + self.ClassCSC114)
+  print("class : " +self.ClassCSC116)
+  print(" " +self.ClassHST)
+  print("         " +self.Summer)
+  print(" " +self.Summer1)
+
+
+
+r2 = Second()
+r2.SecondSemester = "Second Semester"
+r2.ClassCSC113 = "Introduction to Linux/Unix Operating System (4 credits)"
+r2.ClassCSC114 = "Computer Networks (4 credits)"
+r2.ClassCSC116 = "Introduction to Computer and Network Security (4 credits)"
+r2.ClassHST = "Quantitative Knowledge & Skills or Scientific Knowledge & Reasoning mathematics or lab science (4 credits)"
+r2.Summer = "Summer Session"
+r2.Summer1 = "Historical Perspective requirement (3 credits)"
+r2.introduce_self()
+
+
+
+class Third:
+ def introduce_self(self):
+  print("             " + self.ThirdSemester)
+  print(" " +self.CSC214)
+  print(" "+self.CSC226)
+  print(" "+self.CSC253)
+  print(" "+self.SHB)
+
+
+
+r3 = Third()
+r3.ThirdSemester = "Third Semester"
+r3.CSC214 = "Computer Networks II (4 credits)"
+r3.CSC226 = "Network Defence & Counter Measures (4 credits)"
+r3.CSC253 = "Introduction to System & Cloud Administration (4 credits)"
+r3.SHB = "Society & Human Behavior requirement (3 credits)"
+r3.introduce_self()
+
+
+class Fourth:
+ def introduce_self(self):
+  print("             " + self.FourthSemester)
+  print(" " + self.HIT102)
+  print(" " + self.CSC230)
+  print(" " + self.CSC250)
+  print(" " + self.CSC260)
+  print(" " + self.ENG102)
+
+
+r4 = Fourth()
+r4.FourthSemester = "Fourth Semester"
+r4.HIT102 = "Approved Ethics Course (HIT 102 Introduction to Customer Service, Ethics & Compliance in Healthcare Technology is recommended (3- or 4-credit course)"
+r4.CSC230 = "Computer & Internet Forensics (4 credits)"
+r4.CSC250 = "IT Capstone Project (3 credits)"
+r4.CSC260 = "Internship, or Approved Technical Course (3– or 4-credit course)"
+r4.ENG102 = "College Composition II or ENG 105 Technical Writing (3 credits) "
+r4.introduce_self()
+
+
+
